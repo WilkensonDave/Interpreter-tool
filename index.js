3ad72015e7258e1c76dfe512c2cf13462c878255
Devariste <devaristewilkenson@gmail.com>
2026-04-30 22:01:04 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+let terms = []
+
+fetch("terms.json").then(response => response.json()).then(data =>{
+    terms = data;
+    displayResults(terms)
+}).catch(error =>{
+    throw error;
+});
+
+
+const form = document.getElementById("searchForm");
+const searchInput = document.getElementById("search");
+const resultsDiv = document.getElementById("results");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // prevent page reload
+
+  const query = searchInput.value.toLowerCase().trim();
+
+  if (!query) {
+    resultsDiv.innerHTML = "<p>Please enter a word</p>";
+    return;
+  }
+
+  const filtered = terms.filter(term =>
+    term.english.toLowerCase().includes(query) ||
+    term.creole.toLowerCase().includes(query) ||
+    term.spanish.toLowerCase().includes(query)
+  );
+
+  displayResults(filtered);
+});
+
+function displayResults(data) {
+  resultsDiv.innerHTML = "";
+
+  if (data.length === 0) {
+    resultsDiv.innerHTML = "<p>No results found</p>";
+    return;
+  }
+
+  data.forEach(term => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+      <h3>English: ${term.english}</h3>
+      <p>Creole: ${term.creole}</p>
+      <p>Spanish: ${term.spanish}</p>
+      <span>${term.category}</span>
+    `;
+
+    resultsDiv.appendChild(card);
+  });
+}
+
+// Search
+searchInput.addEventListener("input", () => {
+  const query = searchInput.value.toLowerCase();
+
+  const filtered = terms.filter(term =>
+    term.english.toLowerCase().includes(query) ||
+    term.creole.toLowerCase().includes(query) ||
+    term.spanish.toLowerCase().includes(query) ||
+    term.category.toLowerCase().includes(query)
+  );
+
+  displayResults(filtered);
+});
+
+// Filter buttons
+function filterCategory(category) {
+  if (category === "All") {
+    displayResults(terms);
+  } else {
+    const filtered = terms.filter(term => term.category === category);
+    displayResults(filtered);
+  }
+}
+
+// Load all initially
+displayResults(terms);
---
+++ readme.md
@@ -0,0 +1,3 @@
+![Alt text](images/intepreter.PNG)
+
+I have create this tool to make my job easier and also to help my friends.
